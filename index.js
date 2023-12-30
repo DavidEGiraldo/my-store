91ea7544796651b1d8b17604650c7a2693d85253
David Giraldo <degiraldod@gmail.com>
2023-12-30 18:24:56 -0500

    feat: add CORS middleware

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const routerApi = require('./routes');
 
 const {
@@ -8,9 +10,21 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello, my server on Express.js');
