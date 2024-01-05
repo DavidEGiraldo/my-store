d5aac1f0d5610658ad2d76992eed88e9a265d966
David Giraldo <degiraldod@gmail.com>
2024-01-04 19:05:42 -0500

    feat: add dotenv, sequelize-cli and sequelize migrations

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,15 +3,18 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const email = Joi.string().email();
 const password = Joi.string().min(8);
+const role = Joi.string().valid('customer', 'admin')
 
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
+  role: role,
 });
 
 const updateUserSchema = Joi.object({
   email: email,
   password: password,
+  role: role
 });
 
 const getUserSchema = Joi.object({
